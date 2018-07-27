41ee426610bce7c62fced320c4969d723bc44b77
Erwin Ernst Steinhammer <eestoneee@gmail.com>
2018-07-27 13:14:21 +0200

    use an better cli browser

--- gov-crawl.sh
+++ gov-crawl.sh
@@ -9,30 +9,32 @@ Regv_par='?Applikation=Regv&DokumenteProSeite=Ten'
 #parsing the law.html from RIS
 function save_law {
   temp_law_filename=$1
-  temp_law_data=$2
-  temp_law_data=${temp_law_data:1:(-1)}
+  temp_law_url=$2
+  temp_law_url=${temp_law_data:1:(-1)}
 
-  echo 'get data from '$temp_law_data
+  echo 'get data from '$temp_law_url
 
-  echo 'save data to '$temp_law_filename
-  curl -o $temp_law_filename $temp_law_data
-  xmlstarlet ed --inplace -N x="http://www.w3.org/1999/xhtml" -d "//x:head" $temp_law_filename
-  xmlstarlet ed --inplace -d "//@class" $temp_law_filename
+  echo 'save data to '$temp_law_filename".html"
+  curl -o $temp_law_filename".html" $temp_law_url
+
+  maxWidth=$(echo "2^(32-1)-1" | bc)
+  html2text -width $maxWidth -o $temp_law_filename".txt" $temp_law_filename".html"
 
   echo -e 'done temp_law_filename'
 }
 
 #RegV
-Regv_tempdata="\"http://www.ris.bka.gv.at/Dokumente/RegV/REGV_COO_2026_100_2_1516300/REGV_COO_2026_100_2_1516300.html\""
-Regv_tempname="./data/REGV_COO_2026_100_2_1516300"".html"
-save_law $Regv_tempname $Regv_tempdata
+Regv_url="\"http://www.ris.bka.gv.at/Dokumente/RegV/REGV_COO_2026_100_2_1516300/REGV_COO_2026_100_2_1516300.html\""
+Regv_filename="./data/REGV_COO_2026_100_2_1516300"
+save_law $Regv_filename $Regv_url
 echo -e 'done RegV'
 
 #Begut
-Begut_tempdata="\"http://www.ris.bka.gv.at/Dokumente/Begut/BEGUT_COO_2026_100_2_1516300/BEGUT_COO_2026_100_2_1516300.html\""
-Begut_tempname="./data/BEGUT_COO_2026_100_2_1516300"".html"
-save_law $Begut_tempname $Begut_tempdata
+Begut_url="\"http://www.ris.bka.gv.at/Dokumente/Begut/BEGUT_COO_2026_100_2_1516300/BEGUT_COO_2026_100_2_1516300.html\""
+Begut_filename="./data/BEGUT_COO_2026_100_2_1516300"
+save_law $Begut_filename $Begut_url
 echo -e 'done Begut'
 
-diff -w -B $Begut_tempname $Regv_tempname > ./data/test.diff
+diff -w -B $Begut_filename".txt" $Regv_filename".txt" > ./data/test.diff
+#diff -w -B $Begut_tempname".html" $Regv_tempname".html" > ./data/test.diff
 diffstat ./data/test.diff
