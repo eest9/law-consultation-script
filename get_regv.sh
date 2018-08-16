ea6abda1925ac552ae6754edc7eb527ac2b964ab
Erwin Ernst Steinhammer <eestoneee@gmail.com>
2018-08-16 16:22:21 +0200

    save data as csv

--- get_regv.sh
+++ get_regv.sh
@@ -5,22 +5,24 @@ Regv_par='?Applikation=Regv&BeschlussdatumVon=2013-10-29&BeschlussdatumBis=2017-
 
 #curl -o ./data/Regv_data.json 'https://data.bka.gv.at/ris/api/v2.5/bundesgesetzblaetter'$Regv_par
 ii=1
-while [  $ii -lt 5 ]; do
+while [  $ii -lt 6 ]; do
 
   curl -o './data/Regv_data-'$ii'.json' 'https://data.bka.gv.at/ris/api/v2.5/bundesgesetzblaetter'$Regv_par'&Seitennummer='$ii
 
   i=0
-  while [  $i -lt 1 ]; do
-      echo '\nThe counter is $i'
+  while [  $i -lt 100 ]; do
+    echo 'The counter is '$i
 
-      Regv_name="$(cat './data/Regv_data-'$ii'.json' | jq '.OgdSearchResult.OgdDocumentResults.OgdDocumentReference['$i'].Data.Metadaten.Bundesgesetzblaetter.Titel')"
-      echo $Regv_name
-      Regv_url_all="$(cat './data/Regv_data-'$ii'.json' | jq '.OgdSearchResult.OgdDocumentResults.OgdDocumentReference['$i'].Data.Metadaten.Allgemein.DokumentUrl')"
-      echo $Regv_url_all
-      Regv_url_pdf="$(cat './data/Regv_data-'$ii'.json' | jq '.OgdSearchResult.OgdDocumentResults.OgdDocumentReference['$i'].Data.Dokumentliste.ContentReference[0].Urls.ContentUrl[1].Url')"
-      echo $Regv_url_pdf
+    Regv_name="$(cat './data/Regv_data-'$ii'.json' | jq '.OgdSearchResult.OgdDocumentResults.OgdDocumentReference['$i'].Data.Metadaten.Bundesgesetzblaetter.Titel')"
+    echo $Regv_name
+    Regv_url_all="$(cat './data/Regv_data-'$ii'.json' | jq '.OgdSearchResult.OgdDocumentResults.OgdDocumentReference['$i'].Data.Metadaten.Allgemein.DokumentUrl')"
+    echo $Regv_url_all
+    Regv_url_pdf="$(cat './data/Regv_data-'$ii'.json' | jq '.OgdSearchResult.OgdDocumentResults.OgdDocumentReference['$i'].Data.Dokumentliste.ContentReference[0].Urls.ContentUrl[1].Url')"
+    echo $Regv_url_pdf
 
-      let i=i+1
+    echo $Regv_name";"$Regv_url_all";"$Regv_url_pdf >> ./data/RegV_sample.csv
+
+    let i=i+1
   done
 
   let ii=ii+1
