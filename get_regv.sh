170053f809f332a8bf251da9c1f7850ad0efee59
Erwin Ernst Steinhammer <eestoneee@gmail.com>
2018-08-17 18:30:05 +0200

    refuctoring

--- get_regv.sh
+++ get_regv.sh
@@ -2,24 +2,26 @@
 #parmeters for the RIS API
 Regv_par='?Applikation=Regv&BeschlussdatumVon=2013-10-29&BeschlussdatumBis=2017-11-08&DokumenteProSeite=OneHundred'
 
+mkdir ./data/regv/
+
 for ii in $(seq 1 5); do
 
   #RIS API request for all
-  curl -o './data/Regv_data-'$ii'.json' 'https://data.bka.gv.at/ris/api/v2.5/bundesgesetzblaetter'$Regv_par'&Seitennummer='$ii
+  curl -o './data/regv/Regv_data-'$ii'.json' 'https://data.bka.gv.at/ris/api/v2.5/bundesgesetzblaetter'$Regv_par'&Seitennummer='$ii
 
-  for i in $(seq 0 100); do
+  for i in $(seq 0 99); do
     echo 'read and write Dataset number '$i' of side '$ii
 
     #read all the titeles and uris of all the bills in the last legislativ periode
-    Regv_name="$(cat './data/Regv_data-'$ii'.json' | jq '.OgdSearchResult.OgdDocumentResults.OgdDocumentReference['$i'].Data.Metadaten.Bundesgesetzblaetter.Titel')"
+    Regv_name="$(cat './data/regv/Regv_data-'$ii'.json' | jq '.OgdSearchResult.OgdDocumentResults.OgdDocumentReference['$i'].Data.Metadaten.Bundesgesetzblaetter.Titel')"
     echo $Regv_name
-    Regv_url_all="$(cat './data/Regv_data-'$ii'.json' | jq '.OgdSearchResult.OgdDocumentResults.OgdDocumentReference['$i'].Data.Metadaten.Allgemein.DokumentUrl')"
+    Regv_url_all="$(cat './data/regv/Regv_data-'$ii'.json' | jq '.OgdSearchResult.OgdDocumentResults.OgdDocumentReference['$i'].Data.Metadaten.Allgemein.DokumentUrl')"
     echo $Regv_url_all
-    Regv_url_pdf="$(cat './data/Regv_data-'$ii'.json' | jq '.OgdSearchResult.OgdDocumentResults.OgdDocumentReference['$i'].Data.Dokumentliste.ContentReference[0].Urls.ContentUrl[1].Url')"
+    Regv_url_pdf="$(cat './data/regv/Regv_data-'$ii'.json' | jq '.OgdSearchResult.OgdDocumentResults.OgdDocumentReference['$i'].Data.Dokumentliste.ContentReference[0].Urls.ContentUrl[1].Url')"
     echo $Regv_url_pdf
 
     #write csv file
-    echo $Regv_name";"$Regv_url_all";"$Regv_url_pdf >> ./data/RegV_sample.csv
+    echo $Regv_name";"$Regv_url_all";"$Regv_url_pdf >> ./data/regv/RegV_sample.csv
 
   done
 
