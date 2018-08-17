13124f80deb36c1558ea3e25645219e48ba30115
Erwin Ernst Steinhammer <eestoneee@gmail.com>
2018-08-17 12:34:03 +0200

    refactoring

--- get_regv.sh
+++ get_regv.sh
@@ -1,18 +1,16 @@
 #!/bin/bash
 #parmeters for the RIS API
 Regv_par='?Applikation=Regv&BeschlussdatumVon=2013-10-29&BeschlussdatumBis=2017-11-08&DokumenteProSeite=OneHundred'
-#Begut_par='?Applikation=Begut&DokumenteProSeite=Ten'
 
-#curl -o ./data/Regv_data.json 'https://data.bka.gv.at/ris/api/v2.5/bundesgesetzblaetter'$Regv_par
-ii=1
-while [  $ii -lt 6 ]; do
+for ii in $(seq 1 5); do
 
+  #RIS API request for all
   curl -o './data/Regv_data-'$ii'.json' 'https://data.bka.gv.at/ris/api/v2.5/bundesgesetzblaetter'$Regv_par'&Seitennummer='$ii
 
-  i=0
-  while [  $i -lt 100 ]; do
-    echo 'The counter is '$i
+  for i in $(seq 0 100); do
+    echo 'read and write Dataset number '$i' of side '$ii
 
+    #read all the titeles and uris of all the bills in the last legislativ periode
     Regv_name="$(cat './data/Regv_data-'$ii'.json' | jq '.OgdSearchResult.OgdDocumentResults.OgdDocumentReference['$i'].Data.Metadaten.Bundesgesetzblaetter.Titel')"
     echo $Regv_name
     Regv_url_all="$(cat './data/Regv_data-'$ii'.json' | jq '.OgdSearchResult.OgdDocumentResults.OgdDocumentReference['$i'].Data.Metadaten.Allgemein.DokumentUrl')"
@@ -20,10 +18,9 @@ while [  $ii -lt 6 ]; do
     Regv_url_pdf="$(cat './data/Regv_data-'$ii'.json' | jq '.OgdSearchResult.OgdDocumentResults.OgdDocumentReference['$i'].Data.Dokumentliste.ContentReference[0].Urls.ContentUrl[1].Url')"
     echo $Regv_url_pdf
 
+    #write csv file
     echo $Regv_name";"$Regv_url_all";"$Regv_url_pdf >> ./data/RegV_sample.csv
 
-    let i=i+1
   done
 
-  let ii=ii+1
 done
