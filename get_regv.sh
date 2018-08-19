8cdf4e7dce9ece3c5853409afbb4b539a8fb407a
Erwin Ernst Steinhammer <eestoneee@gmail.com>
2018-08-19 03:57:51 +0200

    added more datapoints

--- get_regv.sh
+++ get_regv.sh
@@ -4,6 +4,9 @@ Regv_par='?Applikation=Regv&BeschlussdatumVon=2013-10-29&BeschlussdatumBis=2017-
 
 mkdir -p ./data/regv/
 
+#Titleline
+echo "Name;Short Name;Date;URL all;URL html" >> ./data/regv/Regv_sample.csv
+
 #Get the 5 Datasets includet in the research object
 for ii in $(seq 1 5); do
 
@@ -17,13 +20,18 @@ for ii in $(seq 1 5); do
     #read all the titeles and uris of all the bills in the last legislativ periode
     Regv_name="$(cat './data/regv/Regv_data-'$ii'.json' | jq '.OgdSearchResult.OgdDocumentResults.OgdDocumentReference['$i'].Data.Metadaten.Bundesgesetzblaetter.Titel')"
     echo $Regv_name
+    Regv_name_short="$(cat './data/regv/Regv_data-'$ii'.json' | jq '.OgdSearchResult.OgdDocumentResults.OgdDocumentReference['$i'].Data.Metadaten.Bundesgesetzblaetter.Kurztitel')"
+    echo $Regv_name_short
+    Regv_date="$(cat './data/regv/Regv_data-'$ii'.json' | jq '.OgdSearchResult.OgdDocumentResults.OgdDocumentReference['$i'].Data.Metadaten.Allgemein.Geaendert')"
+    Regv_date=${Regv_date:1:(-1)}
+    echo $Regv_date
     Regv_url_all="$(cat './data/regv/Regv_data-'$ii'.json' | jq '.OgdSearchResult.OgdDocumentResults.OgdDocumentReference['$i'].Data.Metadaten.Allgemein.DokumentUrl')"
     echo $Regv_url_all
     Regv_url_html="$(cat './data/regv/Regv_data-'$ii'.json' | jq '.OgdSearchResult.OgdDocumentResults.OgdDocumentReference['$i'].Data.Dokumentliste.ContentReference[0].Urls.ContentUrl[1].Url')"
     echo $Regv_url_html
 
     #write csv file
-    echo $Regv_name";"$Regv_url_all";"$Regv_url_html >> ./data/regv/RegV_sample.csv
+    echo $Regv_name";"$Regv_name_short";"$Regv_date";"$Regv_url_all";"$Regv_url_html >> ./data/regv/Regv_sample.csv
 
   done
 
