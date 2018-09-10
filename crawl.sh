5942ff7a5f7ee561fc627bf590ed6e55ac653fc0
Erwin Ernst Steinhammer <eestoneee@gmail.com>
2018-09-10 02:17:15 +0200

    bugfix

--- crawl.sh
+++ crawl.sh
@@ -11,34 +11,33 @@ function save_law {
   echo 'save data to '$temp_law_filename
 
   echo 'convert html to text'
-  maxWidth=$(echo "2^(32-1)-1" | bc) #max with of cli for the html2text converter
+  maxWidth=$(echo "2^(32-1)-1" | bc) #max width of cli for the html2text converter
   html2text -width $maxWidth -o $temp_law_filename".txt" $temp_law_filename".html"
 
   echo -e 'done '$temp_law_filename
 }
 
-for line in $(seq 0 242); do
+for line in $(seq 1 242); do
   sample_line=$(cat ./data/bills/sample-1-diff.csv | sed -n $line"p")
   echo $sample_line
 
-  mkdir -p "./data/bills/"$(echo $sample_line | csvtool -t ';' col 2 -)"/"
+  parl_nr=$(echo $sample_line | csvtool -t ';' col 2 -)
+  mkdir -p "./data/bills/"$parl_nr"/"
 
   #Begut
   #get the URL of the consultation bills HTML
   Begut_url=$(echo $sample_line | csvtool -t ';' col 3 -)
-  Begut_filename="./data/bills/"$(echo $sample_line | csvtool -t ';' col 2 -)"/Begut"
+  Begut_filename="./data/bills/"$parl_nr"/Begut"
   save_law $Begut_filename $Begut_url
   echo -e 'saved '$Begut_url' in '$Begut_filename
 
   #RegV
   #get the URL of the council bills HTML
   Regv_url=$(echo $sample_line | csvtool -t ';' col 4 -)
-  Regv_filename="./data/bills/"$(echo $sample_line | csvtool -t ';' col 2 -)"/RegV"
+  Regv_filename="./data/bills/"$parl_nr"/RegV"
   save_law $Regv_filename $Regv_url
   echo -e 'saved '$Regv_url' in '$Regv_filename
 
   diff -w -B $Begut_filename".txt" $Regv_filename".txt" > ./data/bills/test.diff
   diffstat -s ./data/bills/test.diff >> ./data/bills/diffstat.csv
-
-  sleep 1s
 done
